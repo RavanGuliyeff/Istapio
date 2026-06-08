3454601e3f98d4f75eaa2e13993e18576794428e
RavanGuliyeff <guliyevtr-ab107@code.edu.az>
2026-06-08 15:09:16 +0400

    feature: Redis configuration ready and SettingService use redis now

---
+++ src/Infrastructure/Istapio.Infrastructure/Services/External/CacheService.cs
@@ -0,0 +1,37 @@
+﻿using Istapio.Application.Services.External.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Istapio.Infrastructure.Services.External;
+
+public class CacheService : ICacheService
+{
+    private readonly IDistributedCache _cache;
+
+    public CacheService(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        var data = await _cache.GetStringAsync(key, cancellationToken);
+        if (data is null) return default;
+        return JsonSerializer.Deserialize<T>(data);
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? expiration = null, CancellationToken cancellationToken = default)
+    {
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = expiration ?? TimeSpan.FromMinutes(10)
+        };
+        var data = JsonSerializer.Serialize(value);
+        await _cache.SetStringAsync(key, data, options, cancellationToken);
+    }
+
+    public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
+    {
+        await _cache.RemoveAsync(key, cancellationToken);
+    }
+}
